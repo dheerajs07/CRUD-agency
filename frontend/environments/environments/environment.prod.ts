1d01a46ef2ce9e4c04c44ba074cb1c3189445a1d
dheerajs07 <153307965+dheerajs07@users.noreply.github.com>
2024-02-20 22:41:15 +0530

    Add files via upload

---
+++ frontend/environments/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://mean-stack-agency-api.vercel.app/v1',
+  providerUrl: '/provider',
+};
---
+++ frontend/environments/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://mean-stack-agency-api.vercel.app/v1',
+  providerUrl: '/provider',
+};
